39ba461885a1df42d63df69a3fe9abd2fdfc686b
Mir M Ali <mir.ali@hpe.com>
2015-09-16 15:56:18 -0500

    REST for writeconfig
    Change-Id: I82606988fde29ae8ac3bd3758f599a9d33ba1040
    Signed-off-by: Mir M Ali <mir.ali@hpe.com>

--- halonrest/urls.py
+++ halonrest/urls.py
@@ -2,5 +2,6 @@ from halonrest.handlers import base
 
 url_patterns = [
         (r'/login', base.LoginHandler),
+        (r'/rest/v1/system/full-configuration', config.ConfigHandler),
         (r'/.*', base.AutoHandler),
 ]
