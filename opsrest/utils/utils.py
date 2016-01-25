1b7c312cb701262f3f4df41c26c408e92b47636c
Scott Gulland <scott.gulland@hpe.com>
2016-01-25 09:33:12 -0800

    chg: dev: Add example audit log wrapper function
    Create an example audit log wrapper function for generating audit log
    events.  This also provides an example pattern for other modules
    to create their own wrapper functions.  This function can be used
    as-is in the restd code or tweaked as needed.
    
    Change-Id: I02ca733ddd57e56fe1816dd084bc38acb6170c91
    Signed-off-by: Scott Gulland <scott.gulland@hpe.com>

--- opsrest/utils/utils.py
+++ opsrest/utils/utils.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+# Copyright (C) 2015-2016 Hewlett Packard Enterprise Development LP
 #
 #  Licensed under the Apache License, Version 2.0 (the "License"); you may
 #  not use this file except in compliance with the License. You may obtain
@@ -20,11 +20,54 @@ import types
 import uuid
 import re
 import urllib
+import audit
 
 from opsrest.resource import Resource
 from opsrest.constants import *
 from opsvalidator import validator
 from tornado.log import app_log
+
+aufd = None
+
+class RequiredParameter(Exception): pass
+
+def audit_log_user_msg(op, cfgdata, user, hostname, addr, result):
+    """
+    Simple wrapper function for audit_log_user_message() from the
+    audit library to insure a consistent audit event message.
+
+    : param op: Text indicating the configuration operation that was
+        performed.  Spaces are not allowed (replace with '-').
+        No other special characters should be used.
+    : param cfgdata: The configuration data that was changed or "None".
+    : param user: The name of the user associated with the REST request.
+    : param hostname: The hostname of local system, None if unknown.
+    : param addr: The network address of the user, None if unknown.
+    : param result: Result of the configuration operation.
+        1 is "success", 0 is "failed"
+    : return: -1 on failure, otherwise the event number.
+    """
+    global aufd
+
+    if (aufd == None):
+        aufd = audit.audit_open()
+
+    cfg = ""
+    if (cfgdata != None):
+        cfg = audit.audit_encode_nv_string("data", str(cfgdata), 0)
+        if (cfg == None):
+            cfg = "out-of-memory!"
+
+    if (op == None):
+        raise RequiredParameter("Missing operation text!")
+
+    if (user == None):
+        raise RequiredParameter("Missing user name!")
+
+    msg = str("op=RESTD:%s %s  user=%s" % (op, cfg, user))
+    res = audit.audit_log_user_message(aufd, audit.AUDIT_USYS_CONFIG,
+                                       msg, hostname, addr, None, result)
+    return res
 
 
 def get_row_from_resource(resource, idl):
