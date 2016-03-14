54ff4ccb8fdb7700633aaa7d319dd83f39d9278c
Madhulika Madishetty <madhulika.madishetty@hpe.com>
2016-03-14 16:25:15 -0700

    fix:usr: Bug #607 - Create SSL PKI during restd init.
    Generate SSL private key and public key (crt file) during
    restd initialization.
    
    Change-Id: Ia91fb8a5699ee8fc71edbf9e5fa12fffe66a782b
    Signed-off-by: Madhulika Madishetty <madhulika.madishetty@hpe.com>

--- restd.py
+++ restd.py
@@ -11,11 +11,43 @@ from opsrest.settings import settings
 from opsrest.application import OvsdbApiApplication
 
 from tornado.log import app_log
+import subprocess
+import os
 
 # enable logging
 from tornado.log import enable_pretty_logging
 options.logging = settings['logging']
 enable_pretty_logging()
+
+SSL_PRIV_DIR = "/etc/ssl/private"
+SSL_PASS_KEY_FILE = "/etc/ssl/private/server.pass.key"
+SSL_PRIV_KEY_FILE = "/etc/ssl/private/server-private.key"
+SSL_CSR_FILE = "/etc/ssl/private/server.csr"
+SSL_CRT_FILE = "/etc/ssl/certs/server.crt"
+
+
+def create_ssl_pki():
+    if not os.path.exists(SSL_PRIV_DIR):
+        os.mkdir(SSL_PRIV_DIR, 0700)
+    passphrase = "pass:"+os.urandom(16)
+    subprocess.call(['openssl', 'genrsa', '-des3', '-passout',
+                     passphrase, '-out', SSL_PASS_KEY_FILE,
+                     '2048'])
+
+    subprocess.call(['openssl', 'rsa', '-passin', passphrase,
+                     '-in', SSL_PASS_KEY_FILE,
+                     '-out', SSL_PRIV_KEY_FILE])
+
+    subprocess.call(['openssl', 'req', '-new', '-key',
+                     SSL_PRIV_KEY_FILE, '-out',
+                     SSL_CSR_FILE, '-subj',
+                     '/C=US/ST=California/L=Palo Alto/O=HPE'])
+
+    subprocess.call(['openssl', 'x509', '-req', '-days', '365', '-in',
+                     SSL_CSR_FILE, '-signkey',
+                     SSL_PRIV_KEY_FILE, '-out',
+                     SSL_CRT_FILE])
+    os.remove(SSL_PASS_KEY_FILE)
 
 
 def main():
@@ -23,9 +55,12 @@ def main():
 
     app_log.debug("Creating OVSDB API Application!")
     app = OvsdbApiApplication(settings)
+
+    create_ssl_pki()
+
     HTTPS_server = tornado.httpserver.HTTPServer(app, ssl_options={
-        "certfile":"/etc/ssl/certs/server.crt",
-        "keyfile":"/etc/ssl/certs/server-private.key"})
+        "certfile": SSL_CRT_FILE,
+        "keyfile": SSL_PRIV_KEY_FILE})
 
     HTTP_server = tornado.httpserver.HTTPServer(app)
 
