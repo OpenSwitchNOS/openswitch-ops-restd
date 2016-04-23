516321843fd3e1cec9d99568415a929915623cbf
Akshar Ranka <akshar.ranka@hpe.com>
2016-04-22 18:08:32 -0700

    fix: usr: bug #114 returning URI in POST requests
    Change-Id: I08fe52f0b2bee87b394bf754ec0c19d1908cd93f
    Signed-off-by: Akshar Ranka <akshar.ranka@hpe.com>

--- opsrest/handlers/ovsdbapi.py
+++ opsrest/handlers/ovsdbapi.py
@@ -135,6 +135,8 @@ class OVSDBAPIHandler(base.BaseHandler):
                                         self.idl)
 
             status = result.status
+            resource_uri = self.request.path + "/" + result.index
+
             if status == INCOMPLETE:
                 self.ref_object.manager.monitor_transaction(self.txn)
                 # on 'incomplete' state we wait until the transaction
@@ -144,6 +146,9 @@ class OVSDBAPIHandler(base.BaseHandler):
 
             # complete transaction
             self.transaction_complete(status)
+
+            # set the http header to include URI
+            self.set_header(HTTP_HEADER_LOCATION, resource_uri)
 
         except APIException as e:
             self.on_exception(e)
