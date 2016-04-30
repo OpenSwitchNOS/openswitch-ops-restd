d68dcfd80a2c41fde7ecc4216221657e1309e9db
Daniela Ramírez <daniela.rch@hpe.com>
2016-04-29 17:59:50 -0700

    Dynamic categories support
    Support for POST, PUT, PATCH operations
    Support recategorize GET results
    
    Dynamic categories details:
    REST should support to change the column category dynamically,
    to do that "category" field at the schema can be a object or
    a string value (configuration, status, statistics)
    If the category value is an object, it must contains the following
    elements:
    * "per-value": matches the possible values with the desired category.
    * "follows": references the column used to define the column category.
    
    TG-838
    
    Change-Id: I0d8541c4514aaa2125e3507910a0ab55c223aacb
    Signed-off-by: Daniela Ramírez <daniela.rch@hpe.com>

---
+++ tests/test_ops-restd_ct_dynamic_categories.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from opsvsi.docker import *
+from opsvsi.opsvsitest import *
+
+import json
+import httplib
+
+from opsvsiutils.restutils.utils import execute_request, login, \
+    get_switch_ip, rest_sanity_check, get_container_id, get_json
+from copy import deepcopy
+from opsvsiutils.restutils.swagger_test_utility import \
+    swagger_model_verification
+
+NUM_OF_SWITCHES = 1
+NUM_HOSTS_PER_SWITCH = 0
+
+PREFIX = "192.168.%d.0/16"
+HTML_SLASH = "%2F"
+
+CONFIG = "configuration"
+STATUS = "status"
+
+base_route_data = {
+    "configuration": {
+        "address_family": "ipv4",
+        "distance": 1,
+        "from": "%(from)",
+        "metric": 0,
+        "prefix": "%(prefix)",
+        "sub_address_family": "unicast"
+    }
+}
+
+insert_route = (
+    '["OpenSwitch",{'
+    '"op": "insert",'
+    '"table": "Route",'
+    '"row": {'
+    '"address_family": "ipv4",'
+    '"distance": 1,'
+    '"from": "%(from)s",'
+    '"metric": 0,'
+    '"prefix": "%(prefix)s",'
+    '"sub_address_family": "unicast",'
+    '"vrf": ["uuid","%(vrf)s"]'
+    '} '
+    '}]'
+    )
+
+ovsdb_client_transact = "ovsdb-client transact '%s'"
+
+
+class myTopo(Topo):
+    def build(self, hsts=0, sws=1, **_opts):
+        self.hsts = hsts
+        self.sws = sws
+        self.switch = self.addSwitch("s1")
+
+
+@pytest.fixture
+def netop_login(request):
+    request.cls.test_var.cookie_header = login(request.cls.test_var.SWITCH_IP)
+
+
+def get_vrf_uuid(switch, vrf_name):
+    cmd_ovsctl = '/usr/bin/ovs-vsctl get VRF ' + vrf_name + ' _uuid'
+    return switch.cmd(cmd_ovsctl).rstrip("\r\n")
+
+
+class DynamicCategoryTest (OpsVsiTest):
+    def setupNet(self):
+        self.net = Mininet(topo=myTopo(hsts=NUM_HOSTS_PER_SWITCH,
+                                       sws=NUM_OF_SWITCHES,
+                                       hopts=self.getHostOpts(),
+                                       sopts=self.getSwitchOpts()),
+                           switch=VsiOpenSwitch, host=None, link=None,
+                           controller=None, build=True)
+
+        self.SWITCH_IP = get_switch_ip(self.net.switches[0])
+        self.PATH = "/rest/v1/system/vrfs/vrf_default/routes"
+        self.cookie_header = None
+
+    def create_static_route(self):
+        info("\n########## Test to Create Static Route "
+             "##########\n")
+        data = deepcopy(base_route_data)
+        prefix = PREFIX % 1
+        data[CONFIG]["from"] = "static"
+        data[CONFIG]["prefix"] = prefix
+        status_code, response_data = execute_request(
+            self.PATH, "POST", json.dumps(data),
+            self.SWITCH_IP, xtra_header=self.cookie_header)
+
+        assert status_code == httplib.CREATED, \
+            "Unexpected status code. Received: %s Response data: %s " % \
+            (status_code, response_data)
+        info("### Static route created ###\n")
+
+        # Verify data
+        new_path = self.PATH + "/" + data[CONFIG]["from"] + "/" \
+            + prefix.replace("/", HTML_SLASH)
+        status_code, response_data = execute_request(
+            new_path, "GET", None, self.SWITCH_IP,
+            xtra_header=self.cookie_header)
+
+        assert status_code == httplib.OK, "Failed to query route"
+        json_data = get_json(response_data)
+
+        assert json_data[CONFIG] == data[CONFIG], \
+            "Configuration data is not equal that posted data"
+        info("### Configuration data validated ###\n")
+
+        info("\n########## End Test to Create Static Route "
+             "##########\n")
+
+    def create_connected_route(self):
+        info("\n########## Test Create Connected Route ##########\n")
+        data = deepcopy(base_route_data)
+        prefix = PREFIX % 2
+        data[CONFIG]["from"] = "connected"
+        data[CONFIG]["prefix"] = prefix
+        status_code, response_data = execute_request(
+            self.PATH, "POST", json.dumps(data),
+            self.SWITCH_IP, xtra_header=self.cookie_header)
+
+        assert status_code == httplib.METHOD_NOT_ALLOWED, \
+            "Unexpected status code. Received: %s Response data: %s " % \
+            (status_code, response_data)
+        info("### Route cannot be created  ###\n")
+
+        info("\n########## End Test Create Connected Route ##########\n")
+
+    def verify_connected_route_categories(self):
+        info("\n########## Test Verify Connected Route Categories "
+             "##########\n")
+        s1 = self.net.switches[0]
+        # Get vrf_default uuid
+        vrf_default_uuid = get_vrf_uuid(s1, "vrf_default")
+        # Set other data
+        prefix = PREFIX % 3
+        from_type = "connected"
+        # Set transaction data
+        transact_data = \
+            insert_route % {"prefix": prefix,
+                            "vrf": vrf_default_uuid,
+                            "from": from_type}
+        cmd_transact = ovsdb_client_transact % transact_data
+        info("Transaction: %s \n" % cmd_transact)
+        info(cmd_transact + "\n")
+        # Execute transaction
+        output = s1.cmd(cmd_transact)
+        output = output.rstrip("\r\n")
+        info("Response output: %s \n" % output)
+
+        # Verify data
+        new_path = self.PATH + "/" + from_type + "/" \
+            + prefix.replace("/", HTML_SLASH)
+
+        status_code, response_data = execute_request(
+            new_path, "GET", None, self.SWITCH_IP,
+            xtra_header=self.cookie_header)
+
+        assert status_code == httplib.OK, "Failed to query route"
+        json_data = get_json(response_data)
+        info("Response data: %s \n " % json_data)
+
+        # Verify if everythin is categorized as status
+        assert "prefix" in json_data[STATUS]
+        assert "address_family" in json_data[STATUS]
+        assert "sub_address_family" in json_data[STATUS]
+        assert "metric" in json_data[STATUS]
+
+        info("\n########## End Test Verify Connected Route Categories "
+             "##########\n")
+
+
+class Test_DynamicCategory:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def setup_class(cls):
+        Test_DynamicCategory.test_var = DynamicCategoryTest()
+        rest_sanity_check(cls.test_var.SWITCH_IP)
+        cls.container_id = get_container_id(cls.test_var.net.switches[0])
+
+    def teardown_class(cls):
+        Test_DynamicCategory.test_var.net.stop()
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    def __del__(self):
+        del self.test_var
+
+    def test_call_create_static_route(self, netop_login):
+        self.test_var.create_static_route()
+        swagger_model_verification(self.container_id,
+                                   "/system/vrfs/{pid}/routes",
+                                   "POST",
+                                   base_route_data)
+
+    def test_call_create_connected_route(self, netop_login):
+        self.test_var.create_connected_route()
+
+    def test_call_verify_connected_route_categories(self, netop_login):
+        self.test_var.verify_connected_route_categories()
