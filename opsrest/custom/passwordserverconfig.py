4e75e396d1f1e77e32d2ac275af35f55d7857238
Ederick Colmenares <ederick.colmenares.sanchez@hpe.com>
2016-05-04 09:30:24 -0700

    Parse Password Server configuration file
    REST parse the Password Server's yaml configuration file to get these
    file's location: ops-passwd-srv.sock, ops-passwd-srv-pub.pem.
    
    TG-834
    Tags: fix, dev
    Change-Id: I54359bdc896d21a27c136be4e980147a5629fbd1
    Signed-off-by: Ederick Colmenares <ederick.colmenares.sanchez@hpe.com>

---
+++ opsrest/custom/passwordserverconfig.py
@@ -0,0 +1,37 @@
+import yaml
+
+from tornado.log import app_log
+
+from opsrest.settings import settings
+from opsrest.constants import (PASSWD_SRV_SOCK_TYPE_KEY,
+                               PASSWD_SRV_PUB_TYPE_KEY)
+
+
+class PasswordServerConfig(object):
+    __instance = None
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(PasswordServerConfig, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self):
+        self.passwd_srv_sock_fd = ''
+        self.passwd_srv_pub_key_loc = ''
+        self.__get_passwd_srv_files_location__()
+
+    def __get_passwd_srv_files_location__(self):
+        try:
+            passwd_srv_yaml = \
+                open(settings['passwd_srv_yaml'], "r")
+            passwd_srv_files = yaml.load_all(passwd_srv_yaml)
+            for file in passwd_srv_files:
+                for k, v in file.items():
+                    passwd_srv_list = v
+            for element in passwd_srv_list:
+                if element['type'] == PASSWD_SRV_SOCK_TYPE_KEY:
+                    self.passwd_srv_sock_fd = element['path']
+                if element['type'] == PASSWD_SRV_PUB_TYPE_KEY:
+                    self.passwd_srv_pub_key_loc = element['path']
+        except IOError as e:
+            app_log.debug("Failed to open Password Server YAML file: %s" % e)
