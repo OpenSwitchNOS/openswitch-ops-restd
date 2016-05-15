3cb881c8fe04d5f1a47f58850276ddd138454b55
Chinh Le <chinh.le@hpe.com>
2016-05-14 18:40:33 -0700

    Merge notifications feature
    Notifications feature merge from the following reviews:
    https://review.openswitch.net/7421
    https://review.openswitch.net/7916
    https://review.openswitch.net/8887
    
    TG-1099
    
    Change-Id: I1145005a8f5ee75562426a7ef3f82fb30a3ef67e
    Signed-off-by: Chinh Le <chinh.le@hpe.com>
    (cherry picked from commit e6a92ae95be287d97275d106b4561e90d2e6342d)

--- opsrest/handlers/base.py
+++ opsrest/handlers/base.py
@@ -14,7 +14,6 @@
 
 import re
 import userauth
-import rbac
 import httplib
 import hashlib
 import json
@@ -24,15 +23,21 @@ from tornado import web
 from tornado import gen
 
 from opsrest.constants import *
-from opsrest.exceptions import APIException, TransactionFailed, \
-    ParameterNotAllowed, NotAuthenticated, \
-    AuthenticationFailed, ForbiddenMethod, \
-    DataValidationFailed
-from opsrest.settings import settings
+from opsrest.exceptions import (
+    APIException,
+    AuthenticationFailed,
+    DataValidationFailed,
+    NotAuthenticated,
+    ParameterNotAllowed,
+    TransactionFailed
+)
 from opsrest.utils.auditlogutils import audit_log_user_msg, audit
 from opsrest.utils.getutils import get_query_arg
 from opsrest.utils.utils import redirect_http_to_https
-
+from opsrest.utils.userutils import (
+    check_authenticated,
+    check_method_permission
+)
 from tornado.log import app_log
 
 
@@ -57,16 +62,10 @@ class BaseHandler(web.RequestHandler):
                           self.request.remote_ip,
                           self.request)
 
-            if settings['auth_enabled'] and self.request.method != "OPTIONS":
-                is_authenticated = userauth.is_user_authenticated(self)
-            else:
-                is_authenticated = True
-
-            if not is_authenticated:
-                raise NotAuthenticated
+            check_authenticated(self, self.request.method)
 
             # Check user's permissions
-            self.check_method_permission()
+            check_method_permission(self, self.request.method)
 
             sort = get_query_arg(REST_QUERY_PARAM_SORTING,
                                  self.request.query_arguments)
@@ -92,11 +91,7 @@ class BaseHandler(web.RequestHandler):
                                      self.request.query_arguments)
             self.validate_selector(selector)
 
-        except APIException as e:
-            self.on_exception(e)
-            self.finish()
-
-        except Exception, e:
+        except Exception as e:
             self.on_exception(e)
             self.finish()
 
@@ -234,22 +229,6 @@ class BaseHandler(web.RequestHandler):
             result = int(200 <= self.get_status() < 300)
             audit_log_user_msg(op, auditlog_type, uri, cfgdata, user,
                                hostname, addr, result, self.error_message)
-
-    def check_method_permission(self):
-
-        method = self.request.method
-
-        # Check permissions only if authentication is enabled
-        # Plus, OPTIONS is allowed for unauthenticated users
-        if method != REQUEST_TYPE_OPTIONS:
-            username = self.get_current_user()
-            if username is None:
-                if settings['auth_enabled']:
-                    raise NotAuthenticated
-            else:
-                permissions = rbac.get_user_permissions(username)
-                if METHOD_PERMISSION_MAP[method] not in permissions:
-                    raise ForbiddenMethod
 
     def validate_selector(self, selector):
         if selector:
