097027d81c05575ee0444d31a97bd9d3e3b74ae7
Zur <zur.nachimovitz@hpe.com>
2016-06-23 14:22:25 -0700

    Reduce show startup config
    TG-1234
    Not loading xml description
    Not loading empty tables
    
    Change-Id: I84a1bb5070946bff40be5cbfb5030b06716cb4dd
    Signed-off-by: Zur <zur.nachimovitz@hpe.com>

--- opslib/apidocgen.py
+++ opslib/apidocgen.py
@@ -1685,7 +1685,7 @@ def getFullAPI(schema):
 
 
 def docGen(schemaFile, xmlFile, title=None, version=None):
-    schema = parseSchema(schemaFile)
+    schema = parseSchema(schemaFile, loadDescription=True)
 
     # Special treat System table as /system resource
     schema.ovs_tables["System"] = schema.ovs_tables.pop("System")
